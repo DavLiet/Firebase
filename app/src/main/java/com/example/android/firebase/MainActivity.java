f86976fce54d3f4032c69052bc44d9f29d4e68c5
DavLiet <davlietjauw@yahoo.com>
2017-07-13 16:26:26 -0500

    Created simple firebase app that sends my initials to FB

--- app/src/main/java/com/example/android/firebase/MainActivity.java
+++ app/src/main/java/com/example/android/firebase/MainActivity.java
@@ -2,14 +2,39 @@ package com.example.android.firebase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mFirebaseBtn;
+
+    private DatabaseReference mDatabase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();  //points towards root of database
+
+        mFirebaseBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //Create a child in root object
+                //Assign some value to the child object
+
+                mDatabase.child("Name").setValue("DLT");
+            }
+        }
+
+        );
     }
 }
 
-//work
