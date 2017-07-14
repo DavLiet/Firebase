899de2ead9a572f39bdcbc2f87c4419dea13e192
DavLiet <davlietjauw@yahoo.com>
2017-07-14 09:33:47 -0500

    Added GoogleAPIClient and Play Services API

--- app/src/main/java/com/example/android/firebase/MainActivity.java
+++ app/src/main/java/com/example/android/firebase/MainActivity.java
@@ -1,19 +1,46 @@
 package com.example.android.firebase;
 
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,LocationListener {
+
+
+    Location mLocation;
+    TextView latLng;
+    GoogleApiClient mGoogleApiClient;
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 15000;
+    private long FASTEST_INTERVAL = 5000;
+
+    private ArrayList permissionsToRequest;
+    private ArrayList permissionsRejected = new ArrayList();
+    private ArrayList permissions = new ArrayList();
+
+    private final static int ALL_PERMISSIONS_RESULT = 101;
+
+
+    //Firebase variables
     private Button mFirebaseBtn;
-
     private DatabaseReference mDatabase;
-
+    /////////////////////////////////
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +62,26 @@ public class MainActivity extends AppCompatActivity {
         }
 
         );
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
     }
 }
 
